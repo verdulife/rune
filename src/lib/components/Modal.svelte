0fa6722ad9f0f4ebb3c8dd8c9f5700a20cd13523
verdulife <verdu@live.com>
2023-08-02 22:46:15 +0200

    yo k se

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let top;
+</script>
+
+<div class="outer" />
+<div class="modal scrollbar" style="--top: {top}%">
+	<h1>Modal</h1>
+</div>
+
+<style>
+	.outer {
+		position: fixed;
+		inset: var(--top) 0 0 0;
+	}
+
+	.modal {
+		position: fixed;
+		inset: var(--top) 0 0 0;
+		background-color: hsl(var(--alt-hsl), 0.75);
+		backdrop-filter: blur(1em);
+		border-radius: 3em 3em 0 0;
+		box-shadow: inset 0px 1px 1px 0px hsl(var(--base-hsl), 0.25);
+		padding: 4em 2em 2em 2em;
+	}
+</style>
--- src/routes/[user]/+page.svelte
+++ src/routes/[user]/+page.svelte
@@ -3,24 +3,42 @@
 	import NoUser from '$components/NoUser.svelte';
 	import Header from '$components/Header.svelte';
 	import CardSlider from '$components/CardSlider.svelte';
+	import Modal from '$components/Modal.svelte';
 
 	export let data;
 	const user = $Users.find((u) => u.name == data.user);
+
+	let top = 100;
 </script>
 
 {#if user}
-<Header name={user.name} />
+	<Header name={user.name} />
 	<section class="col wfull">
 		<CardSlider />
 		<CardSlider />
 		<CardSlider />
+
+		<button>+ AÑADIR</button>
 	</section>
+
+	<Modal {top} />
 {:else}
 	<NoUser />
 {/if}
 
 <style>
 	section {
+		position: relative;
 		gap: 3em;
+		padding-bottom: 8em;
+	}
+
+	button {
+		position: fixed;
+		bottom: 3em;
+		left: 0;
+		right: 0;
+		width: 150px;
+		margin: 0 auto;
 	}
 </style>
